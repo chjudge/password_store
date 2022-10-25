9bcf526958b168373f13fa1f48d1ece0ecf64c56
BROWDERKD20 <BROWDERKD20@gcc.edu>
2022-10-25 14:39:56 -0400

    mostly functional read and write thingies

--- src/password_store.rs
+++ src/password_store.rs
@@ -1,28 +1,40 @@
 use base64;
 use rand::{thread_rng, Rng};
-use sha2::{Sha512, Digest};
-use std::{fs::File, fmt::format, io::Write};
+use sha2::{Digest, Sha512};
+use std::{fmt::format, fs::File, io::Write, fs::OpenOptions};
 
 pub(crate) const PATH: &str = "Project2PW.txt";
 
-// fn read_file() -> Vec<String> {
-//     let s = std::fs::read_to_string(PATH)?.split('\n')
+fn read_file() -> Vec<String> {
+    return std::fs::read_to_string(PATH)
+        .expect("File should exist prior to this being called")
+        .split('\n')
+        .map(|x| x.to_string())
+        .collect();
+}
 
-// }
+fn write_line(line: String) {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open(PATH)
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "{}", line) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+}
 
 //adds a user to the password store
 pub(crate) fn add(user: String, password: String) {
-    println!("add"); 
+    println!("add");
 
     let rand: String = thread_rng()
         .sample_iter(&rand::distributions::Alphanumeric)
         .take(8)
         .map(char::from)
         .collect();
-    
 
     let salt = base64::encode(rand);
-
 
     let mut hasher = Sha512::new();
     hasher.update(password + &salt);
@@ -30,16 +42,7 @@ pub(crate) fn add(user: String, password: String) {
     let hash = base64::encode(hasher.finalize());
 
     let out = format!("{user}:$6${salt}${hash}");
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open("Project2PW.txt") 
-        .unwrap();
-
-    if let Err(e) = writeln!(file, "{}", out) {
-        eprintln!("Couldn't write to file: {}", e);
-    }
-
+    write_line(out);
 }
 
 pub(crate) fn check(user: String, password: String) {
