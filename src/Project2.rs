4dc0fc205195f00fc3254eaa30155855bd42680f
chjudge <judgech19@gcc.edu>
2022-10-25 00:46:28 -0400

    first attempt at creating hash string in add-user

--- src/Project2.rs
+++ src/Project2.rs
@@ -12,6 +12,7 @@ fn main() {
     }
     let mut line: String;
     loop {
+        println!("please enter a command");
         line = String::new();
         let result = std::io::stdin().read_line(&mut line);
 
@@ -21,19 +22,42 @@ fn main() {
                 continue;
             }
             Ok(_) => {
-                let mut args = line.split(" ");
+                let mut args = line.trim().split(" ");
 
                 let cmd = args.next().unwrap();
-                let user = args.next().unwrap_or("*").to_owned();
-                let password = args.next().unwrap_or("*").to_owned();
+                let user = args.next();
+                let password = args.next();
 
                 match cmd {
-                    "add-user" => password_store::add(user, password),
-                    "check-password" => password_store::check(user, password),
-                    "remove-user" => password_store::remove(user),
+                    "add-user" => {
+                        if !user.is_none() && !password.is_none(){
+                            password_store::add(user.unwrap().to_owned(), password.unwrap().to_owned())
+                        } else if !user.is_none(){
+                            eprintln!("Error: No user specified for command 'add-user'")
+                        } else {
+                            eprintln!("Error: No password specified for command 'add-user'")
+                        }
+                    }
+
+                    "check-password" => {
+                        if !user.is_none() && !password.is_none(){
+                            password_store::check(user.unwrap().to_owned(), password.unwrap().to_owned())
+                        } else if !user.is_none(){
+                            eprintln!("Error: No user specified for command 'check-password'")
+                        } else {
+                            eprintln!("Error: No password specified for command 'check-password'")
+                        }
+                    }
+                    "remove-user" => {
+                        if !user.is_none(){
+                            password_store::remove(user.unwrap().to_owned())
+                        } else{
+                            eprintln!("Error: no user specified for command 'remove-user'")
+                        }
+                    },
                     "print" => password_store::print(),
                     "end" => break,
-                    _ => println!("Invalid command"),
+                    _ => eprintln!("Invalid command"),
                 }
             }
         }
--- src/password_store.rs
+++ src/password_store.rs
@@ -1,59 +1,63 @@
+use base64;
+use rand::{thread_rng, Rng};
+use sha2::{Sha512, Digest};
+use std::{fs::File, fmt::format};
+
 pub(crate) const PATH: &str = "Project2PW.txt";
 
-// function that opens the file, reads it, and returns a vector of strings
-// each string is a line in the file
-fn read_file() -> Vec<String> {
-    let mut file = match std::fs::File::open(PATH) {
-        Ok(file) => file,
-        Err(_) => {
-            eprintln!("Error opening file");
-            return Vec::new();
-        }
-    };
+// fn read_file() -> Vec<String> {
+//     let s = std::fs::read_to_string(PATH)?.split('\n')
 
-    let mut contents = String::new();
-    match std::io::Read::read_to_string(&mut file, &mut contents) {
-        Ok(_) => {},
-        Err(_) => {
-            eprintln!("Error reading file");
-            return Vec::new();
-        }
-    }
+// }
 
-    contents.lines().map(|s| s.to_string()).collect()
-}
+// fn write_line(line: String) {
+//     std::fs::write(PATH, line);
+// }
 
 //adds a user to the password store
-pub(crate) fn add(user: String, password: String){
-    println!("add")
+pub(crate) fn add(user: String, password: String) {
+    println!("add");
+
+    let rand: String = thread_rng()
+        .sample_iter(&rand::distributions::Alphanumeric)
+        .take(8)
+        .map(char::from)
+        .collect();
+    
+
+    let salt = base64::encode(rand);
+
+
+    let mut hasher = Sha512::new();
+    hasher.update(password + &salt);
+
+    let hash = base64::encode(hasher.finalize());
+
+    println!("{user}:$6${salt}${hash}");
 }
 
-pub(crate) fn check(user: String, password: String){
+pub(crate) fn check(user: String, password: String) {
     println!("check")
 }
 
-pub(crate) fn remove(user: String){
+pub(crate) fn remove(user: String) {
     println!("remove")
 }
 
-pub(crate) fn print(){
-    let lines = read_file();
+pub(crate) fn print() {
+    // let lines = read_file();
 
-    for line in lines{
-        // println!("{}", line)
-        animate_text(&line, 50)
-    }
-
+    // for line in lines{
+    //     // println!("{}", line)
+    //     animate_text(&line, 50)
+    // }
 }
 
-
-
 // function that animates the text
-pub(crate) fn animate_text(text: &str, speed: u64){
-    for c in text.chars(){
+pub(crate) fn animate_text(text: &str, speed: u64) {
+    for c in text.chars() {
         print!("{}", c);
         std::io::Write::flush(&mut std::io::stdout()).unwrap();
         std::thread::sleep(std::time::Duration::from_millis(speed));
     }
 }
-
